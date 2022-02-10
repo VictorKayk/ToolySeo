8b0c28957730d220357a2f63cbf1f7747ef0ab5f
VictorKayk <victorkayk77@outlook.com>
2022-02-10 19:59:11 -0300

    :wrench: Updating tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ module.exports = {
   ],
   theme: {
     extend: {
-      colors: {},
+      colors: {
+        primary: 'var(--main-color-primary)',
+        secondary: 'var(--main-color-secondary)',
+        'alt-primary': 'var(--alt-color-primary)',
+        'alt-secondary': 'var(--alt-color-secondary)',
+        'color-pink': 'var(--color-pink)',
+        'color-grey': 'var(--color-grey)',
+        'color-grey-70': 'var(--color-grey-70)',
+        'color-white-80': 'var(--color-white-80)',
+        'color-white-50': 'var(--color-white-50)',
+      },
+      width: {
+        maxWidth: 'var(--max-width)',
+      },
     },
   },
   plugins: [],
