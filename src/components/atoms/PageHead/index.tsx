df5ec4294cc4fb092e7c97772a74c7f243360baa
VictorKayk <victorkayk77@outlook.com>
2022-02-16 10:25:47 -0300

    :recycle: Refactoring the page head

---
+++ src/components/atoms/PageHead/index.tsx
@@ -0,0 +1,32 @@
+import { useRouter } from 'next/router';
+import Head from 'next/head';
+
+interface PageHeadProps {
+  title: string;
+}
+
+export function PageHead({ title }: PageHeadProps) {
+  const { asPath } = useRouter();
+  console.log(asPath);
+
+  return (
+    <Head>
+      <title>{title}</title>
+      <link
+        rel="alternate"
+        hrefLang="en"
+        href={`${process.env.NEXT_PUBLIC_WEB_URL}${asPath}`}
+      />
+      <link
+        rel="alternate"
+        hrefLang="pt"
+        href={`${process.env.NEXT_PUBLIC_WEB_URL}/pt-BR${asPath}`}
+      />
+      <link
+        rel="alternate"
+        hrefLang="x-default"
+        href={`${process.env.NEXT_PUBLIC_WEB_URL}${asPath}`}
+      />
+    </Head>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,18 @@
 import type { GetStaticProps, NextPage } from 'next';
-import Head from 'next/head';
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { HomeTemplate } from '../components/templates/Home';
 import { Layout } from '../components/organisms/Layouts';
 import { useTools } from '../hooks/useTools';
+import { PageHead } from '../components/atoms/PageHead';
 
 const Home: NextPage = () => {
   const { t } = useTranslation('common');
-
   const tools = useTools();
 
   return (
     <>
-      <Head>
-        <title>{`${t('website-name')} | Home`}</title>
-        <link
-          rel="alternate"
-          hrefLang="en"
-          href={`${process.env.NEXT_PUBLIC_WEB_URL}`}
-        />
-        <link
-          rel="alternate"
-          hrefLang="pt"
-          href={`${process.env.NEXT_PUBLIC_WEB_URL}/pt-BR`}
-        />
-        <link
-          rel="alternate"
-          hrefLang="x-default"
-          href={`${process.env.NEXT_PUBLIC_WEB_URL}`}
-        />
-      </Head>
+      <PageHead title={`${t('website-name')} | Home`} />
       <Layout groups={tools}>
         <HomeTemplate groups={tools} />
       </Layout>
